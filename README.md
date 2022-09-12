12aa029645ac479b33d0f9cbb6f5751b0ef9d96d
Sarah <sarahbridge99@gmail.com>
2022-09-12 15:17:59 +1200

    wk7

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # 212-arcade
 
-##virtual environment -- mac
+## virtual environment -- mac
 python3 -m venv env
 source env/bin/activate
 pip install flask
 
-##virtual environment -- windows
+## virtual environment -- windows
 py -m venv env
 env\scripts\activate.bat
 pip install flask
+
+## running flask -- mac
+export FLASK_APP=run.py
+export FLASK_DEBUG=1
+flask run
+
+## running flask -- windows  
+env\scripts\activate.bat
+set FLASK_APP=run.py
+set FLASK_DEBUG=1
+py -m flask run
+
+
+#command option C, Ctrl shift C, F12 -- developer tools
---
+++ static/js.js
@@ -0,0 +1,91 @@
+
+
+// variables
+
+let colors = [
+  '#D00',
+  '#DD0',
+  '#0DD',
+  '#22D',
+  '#D0D'
+];
+
+let scores = [  //listoflists
+  ['TAB','1991-03-03','12345'],
+  ['BAT','1991-02-02','5000'],
+  ['JEM','1991-01-01','1000'],
+  ['TOB','1991-01-03','1555'],
+  ['BEE','1991-03-02','7000'],
+  ['OOP','1991-02-01','1250'],
+];
+
+let topscore = 13000;
+
+
+
+
+
+//submit button
+
+document.querySelector('#addscore a').addEventListener('click', () => {
+  let entry = [
+    document.getElementById('name').value,
+    document.getElementById('date').value,
+    document.getElementById('score').value
+  ];
+
+  document.querySelector('#addscore .alert').innerHTML = '';
+
+  if (entry[0] == '') {
+    document.querySelector('#addscore .alert').innerHTML = 'RE-ENTER NAME!';
+  }
+  else if (entry[1].search(/[0-9]{4}-[0-9]{2}-[0-9]{2}/) == -1) {
+    document.querySelector('#addscore .alert').innerHTML = 'RE-ENTER DATE!';
+  }
+  else if (entry[2].search(/^[0-9]*$/) < 0 || entry[2] == '') {
+    document.querySelector('#addscore .alert').innerHTML = 'RE-ENTER SCORE!';
+  }
+  else {
+    insertEntry(entry);
+  }
+});
+
+function insertEntry(entry/*arguments go here*/) {
+  scores.push(entry);
+
+  document.getElementById('highscores').innerHTML = '<tr><th>[NAME]</th><th>[DATE]</th><th>[SCORE]</th></tr>';
+  document.getElementById('chart').innerHTML = '';     //retrieve html in highscores section and replace it.
+
+    for (let i=0; i < scores.length; i++) {
+      let color = colors[i%colors.length]; //whole list of colours and will never run out.
+      let bar = document.createElement('div'); //creating a thing called bar
+      bar.className = 'bar';
+      bar.style.width = scores[i][2] / topscore * 100 + '%'; //takes the score in bar and compares to top score to determine % of how wide it is.
+      bar.style.backgroundColor = color;
+      document.getElementById('chart').appendChild(bar);
+
+      let row = document.createElement('tr');
+      row.style.color = color;
+      row.innerHTML += '<td>' + scores[i][0] + '</td>';
+      row.innerHTML += '<td>' + scores[i][1] + '</td>';
+      row.innerHTML += '<td>' + scores[i][2] + '</td>';
+      document.getElementById('highscores').appendChild(row);
+    }
+}
+
+/*
+
+
+
+let bar = document.createElement('div');
+bar.className = 'bar';
+bar.style.width = '95%';
+bar.style.backgroundColor = '#D00';
+document.getElementById('chart').appendChild(bar);
+
+let row = document.createElement('tr');
+row.style.color = '#D00';
+row.innerHTML += '<td>TAB</td>';
+row.innerHTML += '<td>1991-03-03</td>';
+row.innerHTML += '<td>12345</td>';
+document.getElementById('highscores').appendChild(row); */
